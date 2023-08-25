1c1630a1db3a8b21e74b41b308b04b75d941469b
Reginaldo Teixeira <reginaldo.teixeira.ti@gmail.com' git config --global user.name 'Reginaldo Teixeira>
2023-08-25 13:33:40 +0100

    Ticket #0001 Task #0002 - Initial Infra 05

--- iwmoa/src/MOA.API/Controllers/WeatherForecastController.cs
+++ iwmoa/src/MOA.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace MOA.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","ASD"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
