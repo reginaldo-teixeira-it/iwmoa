8018e541326d615781bb2ecdca202be9ad26fb4f
Reginaldo Teixeira <reginaldo.teixeira@together.pt>
2023-08-25 10:52:09 +0100

    Ticket #0001 Task #0002 - Initial Infra 03

--- iwmoa/src/MOA.API/Controllers/WeatherForecastController.cs
+++ iwmoa/src/MOA.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace MOA.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","IW"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
