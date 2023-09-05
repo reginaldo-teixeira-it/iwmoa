481db9a2f6b0e3ac6c8f07cda5a33e861ab81357
Reginaldo Teixeira <reginaldo.teixeira.ti@gmail.com' git config --global user.name 'Reginaldo Teixeira>
2023-09-06 00:29:44 +0100

    Ticket 0001 Task0002 - Configure class program

--- iwmoa/src/MOA.API/Program.cs
+++ iwmoa/src/MOA.API/Program.cs
@@ -1,4 +1,33 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder( args );
+
+
+// Add services
+builder.Services.AddCors();
+builder.Services.AddResponseCompression( options =>
+{
+    options.Providers.Add<GzipCompressionProvider>();
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat( new[] { "application/json" } );
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat( new[] { "image/jpeg", "image/png", "application/font-woff2", "image/svg+xml" } );
+    options.EnableForHttps = true;
+} );
+
+//var key = Encoding.ASCII.GetBytes( Settings.Secret );
+//builder.Services.AddAuthentication( JwtBearerDefaults.AuthenticationScheme )
+//    .AddJwtBearer( x =>
+//    {
+//        x.RequireHttpsMetadata = false;
+//        x.SaveToken = true;
+//        x.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateIssuerSigningKey = true,
+//            IssuerSigningKey = new SymmetricSecurityKey( key ),
+//            ValidateIssuer = false,
+//            ValidateAudience = false
+//        };
+//    } );
 
 // Add services to the container.
 
@@ -6,30 +35,41 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+ 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint.
+app.UseSwagger();
+
+// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+// specifying the Swagger JSON endpoint.
+app.UseSwaggerUI( c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
-    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
-    // specifying the Swagger JSON endpoint.
-    app.UseSwaggerUI( c =>
-    {
-        c.SwaggerEndpoint( "/swagger/v1/swagger.json", "IWMOA API V1" );
-    } );
-}
- 
+    c.SwaggerEndpoint( "/swagger/v1/swagger.json", "MOA API V1" );
+} );
+
+app.UseRouting();
+
+// global cors policy
+app.UseCors( x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader() );
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseResponseCompression();
+app.UseStaticFiles();
+
+app.UseEndpoints( endpoints =>
+{
+    endpoints.MapControllers();
+} );
 
 app.Run();
